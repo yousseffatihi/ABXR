11aa2f30a760087c824fda0fae3c870da8d2eca1
Youssef Fatihi <Usefps@gmail.com>
2022-07-14 16:03:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+string input = "0";
+
+while(input != "99")
+{
+    Console.WriteLine("1. Add a shape.");
+    Console.WriteLine("2. Remove a shape.");
+    Console.WriteLine("3. Store a shape.");
+    Console.WriteLine("4. Edit a shape.");
+    Console.WriteLine("5. Show all stored shapes.");
+    while (true)
+    {
+        input = Console.ReadLine();
+        input = input.Trim();
+        if (input == "0")
+            return;
+
+        if(Regex.Match(input, "^(99)|([0-9])$").Success)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid number, please try again.");
+        }
+    }
+
+    switch (input)
+    {
+        case "1":
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add a shape");
+                //TODO Create a function
+                break;
+            }
+        case "2":
+            {
+                Console.Clear();
+                Console.WriteLine("2. Remove a shape");
+                //TODO Create a function
+                break;
+            }
+        case "3":
+            {
+                Console.Clear();
+                Console.WriteLine("3. Store a shape.");
+                //TODO Create a function
+                break;
+            }
+        case "4":
+            {
+                Console.Clear();
+                Console.WriteLine("4. Edit a shape.");
+                //TODO Create a function
+                break;
+            }
+        case "5":
+            {
+                Console.Clear();
+                Console.WriteLine("5. Show all stored shapes.");
+                //TODO Create a function
+                break;
+            }
+        default:
+            return;
+    }
+}
---
+++ classes/Shape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABXR.classes
+{
+    internal class Shape
+    {
+        private static int counter = 0;
+        private int _id;
+        private float _width;
+        private float _height;
+        private int _sides;
+        private float[,,] _position; 
+        private float _scale;
+        private float[,,] _rotation;
+
+        public Shape(float width, float height, int sides, float[,,] position, float scale, float[,,] rotation)
+        {
+            _id = ++counter;
+            _width = width;
+            _height = height;
+            _sides = sides;
+            _position = position;
+            _scale = scale;
+            _rotation = rotation;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public float Width { get => _width; set => _width = value; }
+        public float Height { get => _height; set => _height = value; }
+        public int Sides { get => _sides; set => _sides = value; }
+        public float[,,] Position { get => _position; set => _position = value; }
+        public float Scale { get => _scale; set => _scale = value; }
+        public float[,,] Rotation { get => _rotation; set => _rotation = value; }
+
+        public override int GetHashCode()
+        {
+            return _id;
+        }
+
+        public override string ToString()
+        {
+            return $"Shape Information : \n" + 
+                $"ID : {_id}\n" + $"Width : {_width}\n" + 
+                $"Height : {_height}\n" + $"Sides : {Sides}\n" +
+                $"Position [x,y,z]: {_position[0, 0, 0]}\n" + 
+                $"Scale : {_scale}\n" + $"Rotation : {_rotation[0, 0, 0]}\n";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var shape = obj as Shape;
+            if (shape == null) return false;
+            return shape._id == _id;
+        }
+    }
+}
